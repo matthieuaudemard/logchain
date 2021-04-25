9a2a27fab02f7cc22fb4d8c48b9f9fcf01ffc1db
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-04-25 04:14:01 +0000

    fix: corrige le problème de connexion à l'api qui pointait sur localhost

--- logchain-front/src/app/services/pipeline.service.ts
+++ logchain-front/src/app/services/pipeline.service.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Pipeline} from '../models/pipeline';
 import {HttpClient} from '@angular/common/http';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PipelineService {
-  private readonly API_URL = 'http://localhost:3333/api';
+
+  private readonly apiUrl = 'http://' + location.hostname + ':3333/api';
 
   constructor(private httpClient: HttpClient) {
   }
@@ -16,6 +18,7 @@ export class PipelineService {
    * Renvoie tous les LogEntry enregistrés sur la blockchain
    */
   public getAll(): Observable<Pipeline[]> {
-    return this.httpClient.get<Pipeline[]>(this.API_URL + '/pipelines');
+    console.log(this.apiUrl);
+    return this.httpClient.get<Pipeline[]>(this.apiUrl + '/pipelines');
   }
 }

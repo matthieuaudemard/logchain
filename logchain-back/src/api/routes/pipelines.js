02b5316367ca70a079258296fe47f802c9872f7b
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-05-09 15:42:31 +0200

    refact: modifie la structure de l'api
     * ajout de l'app.js
     * création de la structure de dossier des routes
     * ajout de la route pipeline

---
+++ logchain-back/src/api/routes/pipelines.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const GITLAB_API_URL = process.env.GITLAB_API_URL;
+const Web3 = require('web3');
+const blockchainAddress = process.env.BLOCKCHAIN_ADDRESS;
+const web3 = new Web3('ws://' + blockchainAddress);
+const request = require('request');
+const Logchain = require('../../abis/Logchain.json');
+const projectToken = process.env.PROJECT_TOKEN;
+const projectId = process.env.PROJECT_ID;
+
+router.get('/', async function (req, res) {
+    const networkId = await web3.eth.net.getId().catch(() => res.status(500).json({msg: 'cannot access to networkId'}));
+    const networkData = Logchain.networks[networkId];
+    console.log("[" + new Date() + "]: received request for pipelines listing");
+    if (networkData) {
+        const logchain = new web3.eth.Contract(Logchain.abi, networkData.address);
+        console.log("[" + new Date() + "]: fetching all pipelines");
+        logchain.getPastEvents('PipelineCreated', {fromBlock: 0, toBlock: 'latest'}).then((events) => {
+            // filtrage des données de l'event pour ne garder que les données des pipelines
+            const pipelines = events.map(event => ({
+                blockId: event.returnValues.blockId,
+                id: event.returnValues.id,
+                status: event.returnValues.status,
+                startedAt: event.returnValues.startedAt,
+                finishedAt: event.returnValues.finishedAt,
+                sha: event.returnValues.sha,
+                ref: event.returnValues.ref
+            }));
+            console.log("[" + new Date() + "]: " + pipelines.length + " received");
+            res.status(200).json(pipelines);
+        }).catch(err => {
+            console.log("[" + new Date() + "]: " + "blockchain unavailable");
+            res.status(500).json(err);
+        });
+    }
+});
+
+router.post('/:id', async function (req, res) {
+    const pipelineId = req.params.id;
+    // Récupération des donées du job depuis l'api GITLAB_API_URL;
+    request({
+        url: GITLAB_API_URL + '/projects/' + projectId + '/pipelines/' + pipelineId,
+        headers: {'Authorization': 'Bearer ' + projectToken},
+        rejectUnauthorized: false
+    }, async function (error, response) {
+        if (!error) {
+            const pipeline = JSON.parse(response.body);
+            const accounts = await web3.eth.getAccounts().catch(() => res.status(500).json({msg: 'error while getting accounts', job: pipeline}));
+            const networkId = await web3.eth.net.getId();
+            const networkData = Logchain.networks[networkId];
+            if (networkData && pipeline && pipeline.id) {
+                const logchain = new web3.eth.Contract(Logchain.abi, networkData.address);
+                // création de pipeline
+                logchain.methods
+                    .createPipeline(pipeline.id, pipeline.status, pipeline.started_at ? pipeline.started_at : '', pipeline.finished_at ? pipeline.finished_at : '', pipeline.sha, pipeline.ref)
+                    .estimateGas() // on évalue le coût en gas de la transaction ...
+                    .then(estimatedGas => {
+                        logchain.methods
+                            .createPipeline(pipeline.id, pipeline.status, pipeline.started_at ? pipeline.started_at : '', pipeline.finished_at ? pipeline.finished_at : '', pipeline.sha, pipeline.ref)
+                            // ... puis on effectue la transaction en précisant le coût en gas estimé
+                            .send({from: accounts[0], gas: estimatedGas})
+                            .then(onResolved => {
+                                    const pipelineCreated = onResolved.events.PipelineCreated.returnValues;
+                                    console.log("[" + new Date() + "]: pipeline created: " + pipelineCreated);
+                                    res.status(201).json(pipelineCreated);
+                                },
+                                () => {
+                                    console.log("[" + new Date() + "]: error while trying to save pipeline");
+                                    res.status(400).json({'msg': 'error while sending transaction'});
+                                }
+                            )
+                            .catch(er => {
+                                console.log("[" + new Date() + "]: " + "unable to send to blockchain");
+                                res.status(500).json({message: 'something went wrong ! find out what 2', er})
+                            });
+                    })
+                    .catch(er => {
+                        console.log("[" + new Date() + "]: " + "unable to compute needed gas");
+                        res.status(500).json({message: 'something went wrong ! find out what 1', er})
+                    });
+            } else {
+                console.log("[" + new Date() + "]: unable to find network data");
+            }
+        } else {
+            console.log("[" + new Date() + "]: " + "unable to reach " + GITLAB_API_URL + " api");
+            res.status(404).json({message: "Unable to reach " + GITLAB_API_URL + " api"});
+        }
+    });
+});
+
+module.exports = router;
